4d04e5eac7c0d9aa4779ea92efd56b2751525b36
Your Name <you@example.com>
2020-12-23 22:29:06 +0530

    inference beta working

--- segmentation_techniques/backend/config.py
+++ segmentation_techniques/backend/config.py
@@ -1,13 +1,7 @@
-MODEL_PATH = "./models/"
+MODEL_PATH = "segmentation_techniques\\backend\\models\\"
 
 STYLES = {
-    "candy": "candy",
-    "composition 6": "composition_vii",
-    "feathers": "feathers",
-    "la_muse": "la_muse",
-    "mosaic": "mosaic",
-    "starry night": "starry_night",
-    "the scream": "the_scream",
-    "the wave": "the_wave",
-    "udnie": "udnie",
+    "Unet": "U-net-efficientnet",
+    "featurepyramidnetwork": "FPN",
+    "linknet": "Linknet",
 }
--- segmentation_techniques/backend/inference.py
+++ segmentation_techniques/backend/inference.py
@@ -1,24 +1,26 @@
+import cv2
+import keras
+import numpy as np
+import matplotlib.pyplot as plt
+
 import config
 import cv2
 import segmentation_models as sm
+#import matplotlib.pyplot as plt # remove pyplot after testing phase over
+#import matplotlib.image as mpimg
+import os
+import albumentations as A
 
-# define network parameters
-n_classes = 1 if len(CLASSES) == 1 else (len(CLASSES) + 1)  # case for binary and multiclass segmentation
-activation = 'sigmoid' if n_classes == 1 else 'softmax'
-
+# important constants
+# TODO:clean code later
 BACKBONE = 'efficientnetb3'
 CLASSES = ['car']
+n_classes = 1 if len(CLASSES) == 1 else (len(CLASSES) + 1)  # case for binary and multiclass segmentation
+activation = 'sigmoid' if n_classes == 1 else 'softmax'
+preprocess_input = sm.get_preprocessing(BACKBONE)
 
-def inference(model, image):
-    model_name = f"{config.MODEL_PATH}{model}.h5"
-    model = sm.Unet(BACKBONE, classes=n_classes, activation=activation)
-    model.load_weights(model_name) 
-    image = np.expand_dims(image, axis=0)
-    pr_mask = model.predict(image).round()
-    image=denormalize(image.squeeze()
-    pr_mask=pr_mask[..., 0].squeeze()
-    return pr_mask
-
+# below was the part of the pipline i used for training.
+# TODO: Replace this pipeline with custom for faster inference.
 # helper function for data visualization
 def visualize(**images):
     """PLot images in one row."""
@@ -39,5 +41,227 @@ def denormalize(x):
     x_min = np.percentile(x, 2)    
     x = (x - x_min) / (x_max - x_min)
     x = x.clip(0, 1)
-    return x    
+    return x
+
+def round_clip_0_1(x, **kwargs):
+    return x.round().clip(0, 1)    
+
+# classes for data loading and preprocessing
+class Dataset:
+    """CamVid Dataset. Read images, apply augmentation and preprocessing transformations.
     
+    Args:
+        images_dir (str): path to images folder
+        masks_dir (str): path to segmentation masks folder
+        class_values (list): values of classes to extract from segmentation mask
+        augmentation (albumentations.Compose): data transfromation pipeline 
+            (e.g. flip, scale, etc.)
+        preprocessing (albumentations.Compose): data preprocessing 
+            (e.g. noralization, shape manipulation, etc.)
+    
+    """
+    
+    CLASSES = ['sky', 'building', 'pole', 'road', 'pavement', 
+               'tree', 'signsymbol', 'fence', 'car', 
+               'pedestrian', 'bicyclist', 'unlabelled']
+    
+    def __init__(
+            self, 
+            images_dir, 
+            masks_dir, 
+            classes=None, 
+            augmentation=None, 
+            preprocessing=None,
+    ):
+        self.ids = os.listdir(images_dir)
+        self.images_fps = [os.path.join(images_dir, image_id) for image_id in self.ids]
+        self.masks_fps = [os.path.join(masks_dir, image_id) for image_id in self.ids]
+        
+        # convert str names to class values on masks
+        self.class_values = [self.CLASSES.index(cls.lower()) for cls in classes]
+        
+        self.augmentation = augmentation
+        self.preprocessing = preprocessing
+    
+    def __getitem__(self, i):
+        
+        # read data
+        image = cv2.imread(self.images_fps[i])
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        mask = cv2.imread(self.masks_fps[i], 0)
+        
+        # extract certain classes from mask (e.g. cars)
+        masks = [(mask == v) for v in self.class_values]
+        mask = np.stack(masks, axis=-1).astype('float')
+        
+        # add background if mask is not binary
+        if mask.shape[-1] != 1:
+            background = 1 - mask.sum(axis=-1, keepdims=True)
+            mask = np.concatenate((mask, background), axis=-1)
+        
+        # apply augmentations
+        if self.augmentation:
+            sample = self.augmentation(image=image, mask=mask)
+            image, mask = sample['image'], sample['mask']
+        
+        # apply preprocessing
+        if self.preprocessing:
+            sample = self.preprocessing(image=image, mask=mask)
+            image, mask = sample['image'], sample['mask']
+            
+        return image, mask
+        
+    def __len__(self):
+        return len(self.ids)
+    
+    
+class Dataloder(keras.utils.Sequence):
+    """Load data from dataset and form batches
+    
+    Args:
+        dataset: instance of Dataset class for image loading and preprocessing.
+        batch_size: Integet number of images in batch.
+        shuffle: Boolean, if `True` shuffle image indexes each epoch.
+    """
+    
+    def __init__(self, dataset, batch_size=1, shuffle=False):
+        self.dataset = dataset
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.indexes = np.arange(len(dataset))
+
+        self.on_epoch_end()
+
+    def __getitem__(self, i):
+        
+        # collect batch data
+        start = i * self.batch_size
+        stop = (i + 1) * self.batch_size
+        data = []
+        for j in range(start, stop):
+            data.append(self.dataset[j])
+        
+        # transpose list of lists
+        batch = [np.stack(samples, axis=0) for samples in zip(*data)]
+        
+        return batch
+    
+    def __len__(self):
+        """Denotes the number of batches per epoch"""
+        return len(self.indexes) // self.batch_size
+    
+    def on_epoch_end(self):
+        """Callback function to shuffle indexes each epoch"""
+        if self.shuffle:
+            self.indexes = np.random.permutation(self.indexes)  
+
+# define heavy augmentations
+def get_training_augmentation():
+    train_transform = [
+
+        A.HorizontalFlip(p=0.5),
+
+        A.ShiftScaleRotate(scale_limit=0.5, rotate_limit=0, shift_limit=0.1, p=1, border_mode=0),
+
+        A.PadIfNeeded(min_height=320, min_width=320, always_apply=True, border_mode=0),
+        A.RandomCrop(height=320, width=320, always_apply=True),
+
+        A.IAAAdditiveGaussianNoise(p=0.2),
+        A.IAAPerspective(p=0.5),
+
+        A.OneOf(
+            [
+                A.CLAHE(p=1),
+                A.RandomBrightness(p=1),
+                A.RandomGamma(p=1),
+            ],
+            p=0.9,
+        ),
+
+        A.OneOf(
+            [
+                A.IAASharpen(p=1),
+                A.Blur(blur_limit=3, p=1),
+                A.MotionBlur(blur_limit=3, p=1),
+            ],
+            p=0.9,
+        ),
+
+        A.OneOf(
+            [
+                A.RandomContrast(p=1),
+                A.HueSaturationValue(p=1),
+            ],
+            p=0.9,
+        ),
+        A.Lambda(mask=round_clip_0_1)
+    ]
+    return A.Compose(train_transform)
+
+
+def get_validation_augmentation():
+    """Add paddings to make image shape divisible by 32"""
+    test_transform = [
+        A.PadIfNeeded(384, 480)
+    ]
+    return A.Compose(test_transform)
+
+def get_preprocessing(preprocessing_fn):
+    """Construct preprocessing transform
+    
+    Args:
+        preprocessing_fn (callbale): data normalization function 
+            (can be specific for each pretrained neural network)
+    Return:
+        transform: albumentations.Compose
+    
+    """
+    
+    _transform = [
+        A.Lambda(image=preprocessing_fn),
+    ]
+    return A.Compose(_transform)
+
+def inference(model, image):
+    # wrap our image inside the Dataset wrapper used for training,
+    # TODO: remove this and add custom pipeline for preprocessing.
+
+    trial_dataset = Dataset(
+    r"segmentation_techniques\backend\trial_dataset", 
+    r"segmentation_techniques\backend\trial_dataset", 
+    classes=CLASSES, 
+    augmentation=get_validation_augmentation(),
+    preprocessing=get_preprocessing(preprocess_input),
+    )
+
+    # configure and load model
+    model = config.STYLES["Unet"]
+    model_name = f"{config.MODEL_PATH}{model}.h5"
+    print(model_name)
+    model = sm.Unet(BACKBONE, classes=n_classes, activation=activation)
+    model.load_weights(model_name) 
+
+    # trial folder must have only one image. hence the [0]
+    image, gt_mask = trial_dataset[0]
+    image = np.expand_dims(image, axis=0)
+    pr_mask = model.predict(image).round()
+    print(pr_mask.shape)
+    print(pr_mask[0].shape)
+    final_image = pr_mask[...,0][0]
+    print(final_image.shape)
+
+    # DEBUG: 
+    plt.figure()
+    plt.imshow(final_image) 
+    plt.show()  # display it
+
+    return final_image,gt_mask
+
+
+
+
+
+
+
+if __name__=="__main__":
+    inference("unet","test.png")
