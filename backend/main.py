27ddbf3cf46d52ef4ae5144b89547840ab883435
Ashish Surve <ashish.surve@outlook.in>
2021-01-10 14:44:26 +0530

    vercel app

--- backend/main.py
+++ backend/main.py
@@ -72,5 +72,5 @@ async def get_image(style: str, file: UploadFile = File(...)):
     return {"name": name, "time": time.time() - start}
 
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8080)
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", host="0.0.0.0", port=8080)
